5d2989139bf96c12deda1719b31c19054d72af6f
mihaialexandru <alex.mihai.email@gmail.com>
2022-09-09 17:33:40 +0300

    Add 50x50 sketchpad, title, styles

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const gridDimension = 16;
+const gridDimension = 50;
 
 function createGrid(dim) {
     const container = document.getElementById("grid-container");
@@ -6,9 +6,33 @@ function createGrid(dim) {
         for (let col = 0; col < dim; col++) {
             let squareDiv = document.createElement("div");
             squareDiv.className = "square-div";
+            addEventListenersToGrid(squareDiv);
             container.appendChild(squareDiv);
         }
     }
+}
+
+function destroyGrid() {
+    const squareDivs = document.getElementsByClassName("square-div");
+    let squareDivsArray = Array.from(squareDivs);
+    squareDivsArray.forEach(squareDiv => removeEventListenersFromGrid);
+}
+
+function gridHoverEvent(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function addEventListenersToGrid(squareDiv) {
+    squareDiv.addEventListener("mousedown", gridHoverEvent);
+    squareDiv.addEventListener("mouseover", e => {
+        if (e.buttons == 1) {
+            gridHoverEvent(e);
+        }
+    });
+}
+function removeEventListenersFromGrid() {
+    squareDiv.removeEventListener("mousedown", gridHoverEvent);
+    squareDiv.removeEventListener("mouseover", gridHoverEvent);
 }
 
 createGrid(gridDimension);
